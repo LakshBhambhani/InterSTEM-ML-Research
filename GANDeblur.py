4ff8a0657f6193515099e457410ca8e0c0455ce4
Laksh Bhambhani <lakshbh@gmail.com>
2021-01-03 07:16:18 -0800

    updates

--- GANDeblur.py
+++ GANDeblur.py
@@ -10,9 +10,16 @@ import time
 from IPython import display
 import pickle
 from PIL import Image
+import cv2
+from sklearn.model_selection import train_test_split
+
 
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
+
+height = 50
+width = 50
+dim = (width, height)
 
 def make_generator_model():
     model = tf.keras.Sequential()
@@ -20,8 +27,6 @@ def make_generator_model():
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
 
     model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
     assert model.output_shape == (None, 7, 7, 128)
@@ -54,31 +59,40 @@ def make_discriminator_model():
 
     return model
 
+blurred = os.listdir('./data/blurred/')
+blurred.sort()
+
+sharp = os.listdir('./data/sharp/')
+sharp.sort()
+
+x_blur = []
+for i in range(len(blurred)):
+    image = Image.open('./data/blurred/' + blurred[i])
+    res = cv2.resize(np.float32(image), dim, interpolation=cv2.INTER_LINEAR)
+    x_blur.append(res)
+
+y_sharp = []
+for i in range(len(sharp)):
+    image = Image.open('./data/sharp/' + sharp[i])
+    res = cv2.resize(np.float32(image), dim, interpolation=cv2.INTER_LINEAR)
+    y_sharp.append(res)
+
+(x_train, x_val, y_train, y_val) = train_test_split(x_blur, y_sharp, test_size=0.25)
+
+
+print(len(x_train))
+print(len(y_train))
+
+generator = make_generator_model()
 discriminator = make_discriminator_model()
+
 discriminator.save_weights('discriminator.h5')
 
-generator = make_generator_model()
 generator.save_weights('generator.h5')
 
-image_files = []
-
-for origImg in glob.glob('./LungCancerDetection/data/test/*'):
-    # image = Image.open(origImg)
-    height = 220
-    width = 220
-    dim = (width, height)
-    # res = cv2.resize(np.float32(image), dim, interpolation=cv2.INTER_LINEAR)
-    image_files.append(origImg)
-
-
-print(len(image_files))
-
-generator = make_generator_model()
-discriminator = make_discriminator_model()
-
-gan = simple_gan(generator, discriminator, normal_latent_sampling((100,)))
-model = AdversarialModel(base_model=gan,player_params=[generator.trainable_weights, discriminator.trainable_weights])
-model.adversarial_compile(adversarial_optimizer=AdversarialOptimizerSimultaneous(), player_optimizers=['adam', 'adam'], loss='binary_crossentropy')
+# gan = simple_gan(generator, discriminator, normal_latent_sampling((100,)))
+# model = AdversarialModel(base_model=gan,player_params=[generator.trainable_weights, discriminator.trainable_weights])
+# model.adversarial_compile(adversarial_optimizer=AdversarialOptimizerSimultaneous(), player_optimizers=['adam', 'adam'], loss='binary_crossentropy')
 
 # history = model.fit(x=train_x, y=gan_targets(train_x.shape[0]), epochs=10, batch_size=batch_size)
 
